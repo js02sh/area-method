9783485e3a05d2d8c47da40dc4830c7a2a2ec40c
Jisang Yun <journeyofbabo@gmail.com>
2023-11-28 10:42:23 +0900

    add Circle

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,118 @@
 use std::io;
+use std::f32::consts::PI;
 
+// Define a struct for a Rectangle
 struct Rectangle {
-    width: f32, 
-    height: f32,
-}
-
-impl Rectangle {
-    fn new(width: f32, height: f32) -> Rectangle{
-        Rectangle { 
-            width, 
-            height, 
-        }
-    }
-    fn area(&self) -> f32 {
-        self.width * self.height
-    }
-    fn show(&self) {
-        println!("Area of {}x{} is {}", self.width, self.height, self.area());
-    }
-}
-
-struct Triangle {
     width: f32,
     height: f32,
 }
 
-impl Triangle {
-    fn new(width: f32, height: f32) -> Triangle{
-        Triangle { 
-            width, 
-            height, 
+// Implement methods for the Rectangle struct
+impl Rectangle {
+    // Constructor to create a new Rectangle
+    fn new(width: f32, height: f32) -> Rectangle {
+        Rectangle {
+            width,
+            height,
         }
     }
+    // Method to calculate the area of the Rectangle
+    fn area(&self) -> f32 {
+        self.width * self.height
+    }
+    // Method to display information about the Rectangle
+    fn show(&self) {
+        println!("Area of {}x{} is {}", self.width, self.height, self.area());
+    }
+}
+// Define a struct for a Triangle
+struct Triangle {
+    width: f32,
+    height: f32,
+}
+// Implement methods for the Triangle struct
+impl Triangle {
+    // Constructor to create a new Triangle
+    fn new(width: f32, height: f32) -> Triangle {
+        Triangle {
+            width,
+            height,
+        }
+    }
+    // Method to calculate the area of the Triangle
     fn area(&self) -> f32 {
         self.width * self.height * 0.5
     }
+    // Method to display information about the Triangle
     fn show(&self) {
         println!("Area of Triangle {}x{} is {}", self.width, self.height, self.area());
     }
 }
 
-fn input(ino: &str) -> f32 {
-    println!("{}",ino);
-    let mut s = String::new();
-    io::stdin().read_line(&mut s).expect("Faild to read line");
-    s.trim().parse().expect("Please input valid number")
+struct Circle {
+    diameter: f32,
+}
+// Implement methods for the Triangle struct
+impl Circle {
+    // Constructor to create a new Triangle
+    fn new(diameter: f32) -> Circle {
+        Circle {
+            diameter,
+        }
+    }
+    // Method to calculate the area of the Triangle
+    fn area(&self) -> f32 {
+        self.diameter.powf(2.0) * PI / 4.0
+    }
+    // Method to display information about the Triangle
+    fn show(&self) {
+        println!("Area of Circle diameter{} is {}", self.diameter, self.area());
+    }
+}
+
+// Function to get user input as a floating-point number
+fn input(prompt: &str) -> f32 {
+    println!("{}", prompt);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input.trim().parse().expect("Please input a valid number")
 }
 
 fn main() {
     loop {
         println!("Welcome to the Area Calculator");
-        println!("r: Rectangle // t: Triangle // q: for end");
+        println!("r: Rectangle // t: Triangle // c: Circle // q: for end");
+        
         let mut ty = String::new();
-        io::stdin().read_line(&mut ty)
-            .expect("Faild to read line");
+        io::stdin().read_line(&mut ty).expect("Failed to read line");
         let ty = ty.trim();
+
         match ty {
             "q" => {
-                println!("See you later..");
+                println!("See you later.. bye");
                 break; // Exit the loop if 'q' is entered
             }
             "r" => {
-                println!("Welclome to the Rectangle calculator");
-                let w = input("Write Wtidth:");
+                println!("Welcome to the Rectangle calculator");
+                let w = input("Write Width:");
                 let h = input("Write Height");
-                let r = Rectangle::new(w,h);
+                let r = Rectangle::new(w, h);
                 r.show();
             }
             "t" => {
-                println!("Welclome to the Triangle calculator");
-                let w = input("Write Wtidth:");
+                println!("Welcome to the Triangle calculator");
+                let w = input("Write Width:");
                 let h = input("Write Height");
-                let t = Triangle::new(w,h);
+                let t = Triangle::new(w, h);
                 t.show();
             }
-            _ => print!("bye")
+            "c" => {
+                println!("Welcome to the Circle calculator");
+                let d = input("Write Diameter:");
+                let c = Circle::new(d);
+                c.show();
+            }
+            _ => print!("Please select others")
         }
-
-        
     }
 }

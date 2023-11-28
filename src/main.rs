b92d21371540398a06b854165d7b3e14b80a0845
Jisang Yun <journeyofbabo@gmail.com>
2023-11-28 10:18:40 +0900

    add input function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::io;
 
 struct Rectangle {
-    width: u32, 
-    height: u32,
+    width: f32, 
+    height: f32,
 }
 
 impl Rectangle {
-    fn new(width: u32, height: u32) -> Rectangle{
+    fn new(width: f32, height: f32) -> Rectangle{
         Rectangle { 
             width, 
             height, 
         }
     }
-    fn area(&self) -> u32 {
+    fn area(&self) -> f32 {
         self.width * self.height
     }
     fn show(&self) {
@@ -40,18 +40,25 @@ impl Triangle {
     }
 }
 
+fn input(ino: &str) -> f32 {
+    println!("{}",ino);
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("Faild to read line");
+    s.trim().parse().expect("Please input valid number")
+}
+
 
 fn main() {
-    let o = Rectangle {
-        width: 10,
-        height: 12,
-    };
-    let j = Rectangle::new(11,12);
-    let k = Triangle::new(12.0,23.0);
+    // let o = Rectangle {
+    //     width: 10,
+    //     height: 12,
+    // };
+    // let j = Rectangle::new(11,12);
+    // let k = Triangle::new(12.0,23.0);
 
-    println!("Area of {}x{} is {}", o.width, o.height, o.width*o.height);
-    j.show();
-    k.show();
+    // println!("Area of {}x{} is {}", o.width, o.height, o.width*o.height);
+    // j.show();
+    // k.show();
 
     loop {
         println!("Welcome to the Area Calculator");
@@ -59,7 +66,6 @@ fn main() {
         let mut ty = String::new();
         io::stdin().read_line(&mut ty)
             .expect("Faild to read line");
-
         let ty = ty.trim();
         match ty {
             "q" => {
@@ -68,28 +74,32 @@ fn main() {
             }
             "r" => {
                 println!("Welclome to the Rectangle calculator");
-                println!("Write Width:");
-                let mut w = String::new();
-                io::stdin().read_line(&mut w).expect("Faild to read line");
-                let w: u32 = w.trim().parse().expect("input num pls");
-                println!("Write Height:");
-                let mut h = String::new();
-                io::stdin().read_line(&mut h).expect("Faild to read line");
-                let h: u32 = h.trim().parse().expect("input num pls");
+                let w = input("Write Wtidth:");
+                // println!("Write Width:");
+                // let mut w = String::new();
+                // io::stdin().read_line(&mut w).expect("Faild to read line");
+                // let w: u32 = w.trim().parse().expect("input num pls");
+                // println!("Write Height:");
+                // let mut h = String::new();
+                let h = input("Write Height");
+                // io::stdin().read_line(&mut h).expect("Faild to read line");
+                // let h: u32 = h.trim().parse().expect("input num pls");
 
                 let r = Rectangle::new(w,h);
                 r.show();
             }
             "t" => {
                 println!("Welclome to the Triangle calculator");
-                println!("Write Width:");
-                let mut w = String::new();
-                io::stdin().read_line(&mut w).expect("Faild to read line");
-                let w: f32 = w.trim().parse().expect("input num pls");
-                println!("Write Height:");
-                let mut h = String::new();
-                io::stdin().read_line(&mut h).expect("Faild to read line");
-                let h: f32 = h.trim().parse().expect("input num pls");
+                let w = input("Write Wtidth:");
+                let h = input("Write Height");
+                // println!("Write Width:");
+                // let mut w = String::new();
+                // io::stdin().read_line(&mut w).expect("Faild to read line");
+                // let w: f32 = w.trim().parse().expect("input num pls");
+                // println!("Write Height:");
+                // let mut h = String::new();
+                // io::stdin().read_line(&mut h).expect("Faild to read line");
+                // let h: f32 = h.trim().parse().expect("input num pls");
 
                 let r = Triangle::new(w,h);
                 r.show();
